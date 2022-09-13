95c514fb4952d3f27ddfeb14b01ebe683b1f1b47
武田和也 <you@example.com>
2022-09-13 17:35:55 +0900

    Repositoryアノテーションを明記

--- src/main/java/com/example/its/domain/issue/IssueRepository.java
+++ src/main/java/com/example/its/domain/issue/IssueRepository.java
@@ -2,6 +2,7 @@ package com.example.its.domain.issue;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
@@ -10,6 +11,7 @@ import java.util.List;
  * 「課題」のリポジトリ層を作成
  */
 @Mapper
+@Repository
 public interface IssueRepository {
     @Select("select * from issues")
     List<IssueEntity> findAll();
